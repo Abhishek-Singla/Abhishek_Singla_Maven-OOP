dbe4bd8395a1b62e8f89c48d7389d804fb7ef949
Abhishek Singla <abhishek@example.com>
2026-06-05 15:22:28 +0530

    Add JUnit 5 tests for Student, Chocolate, OtherSweets and Sweets hierarchy

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,77 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.Test;
+class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+    private Chocolate chocolate;
+    private OtherSweets otherSweets;
+    private Sweets sweets;
+    private Student student;
+
+    @BeforeEach
+    void setUp() {
+        chocolate = new Chocolate();
+        chocolate.setCname("DairyMilk");
+        chocolate.setWeight(100);
+
+        otherSweets = new OtherSweets();
+        otherSweets.setName("Ladoo");
+        otherSweets.setWeight(200);
+
+        sweets = new Chocolate();
+        sweets.setChocolate(chocolate);
+        sweets.setOtherSweets(otherSweets);
+
+        student = new Student();
+        student.setName("Abhishek");
+        student.setSweets(sweets);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    void chocolateGetCnameReturnsSetValue() {
+        assertEquals("DairyMilk", chocolate.getCname());
+    }
+
+    @Test
+    void chocolateGetWeightReturnsSetValue() {
+        assertEquals(100, chocolate.getWeight());
+    }
+
+    @Test
+    void otherSweetsGetNameReturnsSetValue() {
+        assertEquals("Ladoo", otherSweets.getName());
+    }
+
+    @Test
+    void otherSweetsGetWeightReturnsSetValue() {
+        assertEquals(200, otherSweets.getWeight());
+    }
+
+    @Test
+    void studentGetNameReturnsSetValue() {
+        assertEquals("Abhishek", student.getName());
+    }
+
+    @Test
+    void studentGetCWReturnsSumOfWeights() {
+        assertEquals(300, student.getCW());
+    }
+
+    @Test
+    void studentGetSweetsReturnsSetSweets() {
+        assertSame(sweets, student.getSweets());
+    }
+
+    @Test
+    void sweetsGetChocolateReturnsSetChocolate() {
+        assertSame(chocolate, sweets.getChocolate());
+    }
+
+    @Test
+    void sweetsGetOtherSweetsReturnsSetOtherSweets() {
+        assertSame(otherSweets, sweets.getOtherSweets());
     }
 }
